e07144b3421b78b84075e12328c4c439bc91c164
Brandon Monier <brandon.monier@gmail.com>
2020-02-27 21:02:47 -0500

    Add new aesthetics and zoom features

--- src/shiny_haplo_viewer.R
+++ src/shiny_haplo_viewer.R
@@ -5,7 +5,7 @@
 # Description:   HaploViewer using Shiny
 # Author:        Brandon Monier
 # Created:       2020-02-27 at 18:41:07
-# Last Modified: 2020-02-27 at 18:48:39
+# Last Modified: 2020-02-27 at 19:24:21
 #--------------------------------------------------------------------
 
 #--------------------------------------------------------------------
@@ -13,6 +13,8 @@
 #    The main purpose of this Rscript is to create a Shiny
 #    application that will project the number of consensus
 #    haplotypes for a given reference range.
+#
+#    NOTE: run `btm_rphg_test.R` first for actual PHG data
 #--------------------------------------------------------------------
 
 # === Preamble ======================================================
@@ -21,42 +23,35 @@
 library(ggthemes)
 library(plotly)
 library(magrittr)
-library(rPHG)
 library(shiny)
 
 
-## Make configuration file ----
-rPHG:::configFileMaker(
-    dbName = "phgSmallSeq.db",
-    dbType = "sqlite",
-    exportPath = paste0(getwd(), "/data")
+
+# === Toy data generation ===========================================
+
+## Parameters ----
+rrSamples <- 300
+
+
+## Random generation of samples (reference ranges) ----
+seqnames <- rep(paste0("1", LETTERS[1:3]), rrSamples / 3)
+seqnames <- seqnames[order(seqnames)]
+
+w <- sample(250:1500, rrSamples, TRUE)
+start <- seq(1, 5e6, by = 3000)[1:300]
+end <- start + w
+
+
+## Data frame generation (S4 Vectors::DataFrame) ----
+testHaplo <- S4Vectors::DataFrame(
+    refRange_id   = factor(paste0("R", seq_len(rrSamples))),
+    seqnames      = factor(seqnames),
+    start         = as.integer(start),
+    end           = as.integer(end),
+    width         = as.integer((end - start) + 1),
+    numHaplotypes = as.integer(sample(1:10, size = rrSamples, replace = TRUE))
 )
-
-
-## Get config file name
-configFilePath <- paste0("data/configFilePHG.txt")
-
-
-## Various graph building methods ----
-method1 <- "GATK_PIPELINE"
-method2 <- "CONSENSUS"
-
-
-
-# === Get PHG data ==================================================
-
-## Start logger ----
-rPHG::startLogger(fullPath = paste0(getwd(), "/output"))
-
-
-## Build the PHG (see rPHG graphBuilder code) ----
-phgObject <- rPHG::graphBuilder(
-    configFile = configFilePath,
-    methods = method2 # CONSENSUS
-)
-
-## Get number of haplotypes in range ----
-numHaplo <- rPHG::numHaploPerRange(phgObject = phgObject)
+rm(w, start, end, seqnames, rrSamples)
 
 
 
@@ -69,14 +64,20 @@ ui <- fluidPage(
 
     shiny::mainPanel(
         ## Drop down for chromosomes
+        # selectInput(
+        #     inputId = "chrom_select",
+        #     label = "Select Chromosome",
+        #     choices = NULL
+        # ),
         selectInput(
-            inputId = "chrom_select",
-            label = "Select Chromosome",
-            choices = NULL
+            "chrom_select",
+            "Select Chromosome",
+            choices = levels(testHaplo$seqnames),
+            selected = levels(testHaplo$seqnames)[1]
         ),
 
         ## Interactive plot declaration
-        plotly::plotlyOutput("hapPlot")
+        div(plotly::plotlyOutput("hapPlot", height = "50%"), align = "center")
     )
 )
 
@@ -86,21 +87,13 @@ server <- function(input, output, session) {
 
     ## Get levels of chromosomes
     choices_chrom <- reactive({
-        as.vector(levels(numHaplo$seqnames))
-    })
-
-    ## Update chromosome selection
-    observe({
-        updateSelectInput(
-            session = session,
-            inputId = "chrom_select",
-            choices = choices_chrom()
-        )
+        as.vector(levels(testHaplo$seqnames))
     })
 
     ## Haploplot
     output$hapPlot <- plotly::renderPlotly({
-        tmp <- as.data.frame(numHaplo)
+        tmp <- as.data.frame(testHaplo)
+        tmp <- tmp[which(tmp$seqnames == as.character(input$chrom_select)), ]
 
         # Shape proportions
         yfrac <- 0.1
@@ -110,8 +103,6 @@ server <- function(input, output, session) {
         tmp$med <- apply(tmp[, 3:4], 1, stats::median)
         tmp$color <- "#91baff"
         tmp[seq(1, nrow(tmp), by = 2),]$color <- "#3e619b"
-
-        tmp$numHaplotypes <- sample(x = 1:5, size = nrow(tmp), replace = TRUE)
 
         # Get limit data
         xbeg <- min(tmp$start)
@@ -124,44 +115,73 @@ server <- function(input, output, session) {
                     xmin = start,
                     xmax = end,
                     ymin = 0,
-                    ymax = max(tmp$numHaplotypes)
+                    ymax = max(tmp$numHaplotypes),
+                    text = paste0(
+                        "<b>Ref Range ID: </b>", refRange_id, "\n",
+                        "<b>Start: </b>", start, "\n",
+                        "<b>Stop: </b>", end
+                    )
                 ),
                 fill = tmp$color,
-                alpha = 0.4
+                alpha = 0.5
             ) +
-            geom_point(aes(med, numHaplotypes, text = paste0("Ref Range ID: ", refRange_id))) +
+            geom_point(
+                mapping = aes(
+                    med,
+                    numHaplotypes,
+                    text = paste0("<b>No. Haplotypes: </b>", numHaplotypes)
+                )
+            ) +
             geom_path(aes(med, numHaplotypes)) +
             xlab("Physical Position (bp)") +
             ylab("Number of Haplotypes")
-        ggplotly(p, tooltip = "text")
 
-        # print(
-        #     ggplotly(
-        #         ggplot(data = tmp) +
-        #             ylim(-(yend * yfrac), yend) +
-        #             scale_x_continuous(limits = c(xbeg, xend)) +
-        #             geom_rect(
-        #                 mapping = aes(
-        #                     xmin = .data$start,
-        #                     xmax = .data$end,
-        #                     ymin = 0,
-        #                     ymax = -(yend * yfrac)
-        #                 ),
-        #                 fill = tmp$color
-        #             ) +
-        #             geom_path(aes(x = .data$med, y = .data$numHaplotypes)) +
-        #             geom_point(
-        #                 aes(
-        #                     x = .data$med,
-        #                     y = .data$numHaplotypes
-        #                 ),
-        #                 size = 1
-        #                 ) +
-        #             facet_grid(seqnames ~ .) +
-        #             xlab("Physical Position (bp)") +
-        #             ylab("Number of Haplotypes")
-        #     )
-        # )
+        ## GG Plotly
+        m <- list(
+            l = 100,
+            r = 10,
+            b = 50,
+            t = 50,
+            pad = 2
+        )
+        ggplotly(p, tooltip = "text") %>%
+            config(displayModeBar = FALSE) %>%
+            layout(
+                autosize = F,
+                xaxis = list(
+                    # rangeselector = list(
+                    #     buttons = list(
+                    #         list(
+                    #             count = 3,
+                    #             label = "3 mo",
+                    #             step = "month",
+                    #             stepmode = "backward"
+                    #         ),
+                    #         list(
+                    #             count = 6,
+                    #             label = "6 mo",
+                    #             step = "month",
+                    #             stepmode = "backward"
+                    #         ),
+                    #         list(
+                    #             count = 1,
+                    #             label = "1 yr",
+                    #             step = "year",
+                    #             stepmode = "backward"
+                    #         ),
+                    #         list(
+                    #             count = 1,
+                    #             label = "YTD",
+                    #             step = "year",
+                    #             stepmode = "todate"),
+                    #         list(step = "all")
+                    #     )
+                    # ),
+                    width = 10000,
+                    rangeslider = list(),
+                    margin = m
+                )
+            )
     })
 
 }
