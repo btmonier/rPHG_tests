4bbcadcef3eae414bc62224102a950010b0c2a01
Brandon Monier <brandon.monier@gmail.com>
2020-02-27 19:08:58 -0500

    Use config file maker

--- src/btm_rphg_tests.R
+++ src/btm_rphg_tests.R
@@ -1,20 +1,37 @@
-# Title:  rPHG flow tests - DO NOT RUN
-# Author: Brandon Monier
+#!/usr/bin/env Rscript
 
-# === Preamble (change depending on user) ===========================
+#--------------------------------------------------------------------
+# Script Name:   btm_rphg_tests.R
+# Description:   rPHG tests for the Wheat PHG workshop (2020)
+# Author:        Brandon Monier
+# Created:       2020-02-27 at 17:49:29
+# Last Modified: 2020-02-27 at 18:25:07
+#--------------------------------------------------------------------
+
+#--------------------------------------------------------------------
+# Detailed Purpose:
+#    The main purpose of this Rscript is to test out new ideas and
+#    workflows for the wheat PHG workshop (2020).
+#
+#    This script makes use of the test small seq database in maize.
+#--------------------------------------------------------------------
+
+# === Preamble ======================================================
 
 ## Load packages ----
 library(rPHG)
 
 
-## Get config path ----
-configFilePath <- system.file(
-    "extdata",
-    "configSQLite.txt",
-    package = "rPHG"
+## Make configuration file ----
+rPHG:::configFileMaker(
+    dbName = "phgSmallSeq.db",
+    dbType = "sqlite",
+    exportPath = paste0(getwd(), "/data")
 )
 
-configFilePath <- "/home/bm646/Temporary/test_rphg/wheat_cap_2019_dbs/congif_sql_btm.txt"
+
+## Get config file name
+configFilePath <- paste0("data/configFilePHG.txt")
 
 
 ## Various graph building methods ----
@@ -26,7 +43,7 @@ method2 <- "CONSENSUS"
 # === Tests - get PHG object and functions ==========================
 
 ## Start logger ----
-rPHG::startLogger(fullPath = "/home/bm646/Temporary/test_rphg/")
+rPHG::startLogger(fullPath = paste0(getwd(), "/output"))
 
 
 ## Get methods ----
@@ -36,10 +53,7 @@ phgMethods <- rPHG::showPHGMethods(configFile = configFilePath)
 ## Build the PHG (see rPHG graphBuilder code) ----
 phgObject <- rPHG::graphBuilder(
     configFile = configFilePath,
-    methods = phgMethods$method_name[5], # CONSENSUS
-    chrom = NULL,
-    includeSequence = FALSE,
-    includeVariant = FALSE
+    methods = method2 # CONSENSUS
 )
 
 
@@ -78,7 +92,7 @@ rmDF <- rPHG::readMappingsForLineName(
 
 ## Number of haplotypes per ref range
 phgObject %>%
-    rPHG::numHaploPerRange(start = 153e6, end = 154e6) %>%
+    rPHG::numHaploPerRange(start = 4e3, end = 5e4) %>%
     rPHG::plotNumHaplo()
 
 
